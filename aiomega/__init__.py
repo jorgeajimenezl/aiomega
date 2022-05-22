061bb252bcde6437a069e21a0ce11b4097eb4857
Jorge Alejandro Jimenez Luna <jorgeajimenezl17@gmail.com>
2022-05-22 17:14:09 -0400

    Fixed packaging issues

--- aiomega/__init__.py
+++ aiomega/__init__.py
@@ -1,4 +1,4 @@
 from aiomega.client import Mega
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Jorge Alejandro Jimenez Luna"
--- configure.sh
+++ configure.sh
@@ -1,3 +1,4 @@
+set -e
 cd sdk
 
 echo "Configuring sdk"
